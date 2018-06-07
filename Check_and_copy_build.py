722d515b5d6ac4e57b359270b2cbf724093b0188
ANikolaev <nikolaxandrew@gmail.com>
2018-06-07 22:48:01 +0300

    added text status and fixed stuff

--- Check_and_copy_build.py
+++ Check_and_copy_build.py
@@ -5,11 +5,12 @@ import os
 import shutil
 import sys
 
-builddir = sys.argv[1]
-copydir = sys.argv[2]
-newdir = str(builddir.replace('by_Freeman_Gordon', 'pkg'))
-newdir = str(newdir.replace('E:\Downloads', ''))
-os.mkdir(copydir + newdir)
+builddir =  sys.argv[1].replace("\\", "/")
+copydir = r'E:/builds/'
+newdir = builddir.replace(r'E:/Downloads/', '')
+newdir = newdir.replace('by_Freeman_Gordon', '_pkg')
+createdir = os.path.join(copydir, newdir)
+os.mkdir(createdir)
 
 def check():
     for file in os.listdir(builddir):
@@ -17,10 +18,12 @@ def check():
         if file.endswith(".mp4") and os.path.getsize(currentFile) > 65000000\
         or file.endswith(".pdf") and os.path.getsize(currentFile) > 200000\
         or file.endswith(".jpg") and os.path.getsize(currentFile) > 16000000:
-            if os.path.exists(copydir + str(newdir) + '/' + file == 0):
-                shutil.copy(currentFile, copydir + str(newdir))
+            if os.path.exists(createdir + file) == 0:
+                print('Copyng ' + file)
+                shutil.copy(currentFile, createdir)
+                print(file + ' copyed!')
 
-while len(os.listdir(copydir + newdir)) != 6:
+while len(os.listdir(createdir)) != 6:
     check()
 else:
-    print('Билдец и exe скопировались, работаем!')
+    print('Build and exe are copyed!')
